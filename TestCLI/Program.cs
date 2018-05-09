3102c1f8721a08c2c7b11a4c6d20645e8e4fde88
Jayant Varma <jayantatjcu@gmail.com>
2018-05-09 19:41:09 +1000

    * .gitignore: First Commit
    * TestCLI.sln:
    * Program.cs:
    * TestCLI.csproj:
    * AssemblyInfo.cs:

---
+++ TestCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCLI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
